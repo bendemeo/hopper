f30d087c5f7e13b29f32818e49b0ea419f32f4b7
Ben DeMeo <bdemeo@mit.edu>
2019-08-06 12:31:53 -0400

    ss

--- treehopper/utils.py
+++ treehopper/utils.py
@@ -14,7 +14,7 @@ def haus_curve(data, ordering, distfunc=euclidean, max_len=5000):
     for i in range(len(ordering)):
         if i > max_len:
             break
-        new = data[ordering[i]]
+        new = list(data[ordering[i]])
         new_dists = pairwise_distances([new], data).flatten()
         min_dists = np.minimum(min_dists, new_dists)
         cur_haus = max(min_dists)
