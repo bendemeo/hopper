9091568316e4d40ef9751967bc53ad6d8c796207
Ben DeMeo <bdemeo@mit.edu>
2019-08-14 12:37:51 -0400

    ss

--- treehopper/explore.py
+++ treehopper/explore.py
@@ -59,8 +59,8 @@ def expand_clusterings(smalldata, fulldata, cluster_name='louvain', clusters = N
         # print(cell)
         inds = np.where([y==cell for y in list(fulldata.obs[vc_name])])[0]
         # print(inds)
-        print(list(smalldata.obs[cluster_name].iloc[i]))
-        print(smalldata.obs[cluster_name])
+        # print(list(smalldata.obs[cluster_name].iloc[i]))
+        # print(smalldata.obs[cluster_name])
         fulldata.obs[cluster_name].iloc[inds]=len(inds)*[smalldata.obs[cluster_name].iloc[i]]
 
     return(fulldata)
